768a2015e9c50bba467e64c309a728723d33797f
Cix8 <francesco.natal_fn@libero.it>
2022-04-07 14:11:40 +0200

    add ticket-price program

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const userKm = parseInt(prompt('Quanti km devi percorrere?'));
+console.log(userKm, typeof(userKm));
+
+const userAge = parseInt(prompt('Quanti anni hai?'));
+console.log(userAge, typeof(userAge));
+
+const priceKm = userKm * 0.21
+console.log(priceKm);
+
+let discount;
+
+if (userAge < 18) {
+    discount = (priceKm * 20) / 100;
+    console.log(discount);
+} else if (userAge > 65) {
+    discount = (priceKm * 40) / 100;
+    console.log(discount);
+} else {
+    discount = 0;
+    console.log(discount);
+}
+
+const finalPrice = priceKm - discount
+console.log(finalPrice);
+
+let message;
+
+if (finalPrice === priceKm) {
+    message = `Il tuo biglietto costa: € ${finalPrice}`
+} else {
+    message = `Il tuo biglietto costa: € ${finalPrice} (è stata applicata una tariffa speciale in base alla tua età!)`
+}
+
+document.getElementById('ticket-price').innerHTML = message
